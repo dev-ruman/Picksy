1dd80bdc483fb976beefe297b1732d2e1e17ea99
- Ruman <sayfurrahman324@gmail.com>
2025-04-24 00:36:23 +0600

    chore(server): add custom port and host configuration
    Configured the app to use a specific hostname and port. Successfully verified the server is listening on the defined settings.

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
 const express = require('express');
+require('dotenv/config');
+
 const app = express();
+const environment = process.env;
+
+const hostname = environment.HOST || '0.0.0.0';
+const port = environment.PORT || 3000;
+
+app.listen(port, hostname, () => {
+  console.log(`Server is running on http://${hostname}:${port}`);
+});
