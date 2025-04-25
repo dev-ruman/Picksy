b8c0e7a7e50be83d684c5c72077733b53b66d15a
- Ruman <sayfurrahman324@gmail.com>
2025-04-26 01:43:47 +0600

    feat(auth): implement registration and login controllers with full route setup
    Added user registration and login controllers, defined routes for /register and /login, integrated validation and error handling.

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 require('dotenv/config');
+
 
 const app = express();
 const environment = process.env;
-
+const api = environment.API_PREFIX || '/api/v1';
 const hostname = environment.HOST || '0.0.0.0';
 const port = environment.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(morgan('tiny'));
+app.use(cors());
+app.options(api, cors());
+
+const authRoutes = require('./routes/auth');
+app.use(`${api}`, authRoutes);
+
+
+mongoose.connect(environment.MONGO_URI).then(() => {
+  console.log('Connected to MongoDB successfully');
+}).catch((err) => {
+  console.error(err);
+});
+
 
 app.listen(port, hostname, () => {
   console.log(`Server is running on http://${hostname}:${port}`);
